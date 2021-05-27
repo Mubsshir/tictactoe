103acdd7b47a1c9988d5fce264a3e4d5ffecc17f
Mubsshir <39831189+Mubsshir@users.noreply.github.com>
2021-05-28 01:14:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 const container = document.querySelector('.box-container');
 const boxes = document.querySelectorAll('.box-container .row .column');
 const body = document.querySelector('body');
-const reset = document.querySelector('.reset button')
+const reset = document.querySelector('.reset button');
 let player = 0;
 let count = 0;
+var win = 0;
 
-function showMessage(message) {
+function showMessage(message, icon) {
     const alertBox = document.createElement('div');
-    alertBox.className = 'message';
-    alertBox.append(document.createTextNode(`${message}`))
+    alertBox.className = `message ${icon}`;
+    alertBox.innerHTML = `<i class ="fab fa-${icon} win"></i>  ${message}`;
     body.insertBefore(alertBox, container);
-
-
+    console.log(alertBox);
     setTimeout(() => {
         alertBox.remove();
         boxes.forEach((box) => {
             box.className = 'column';
         })
     }, 2000);
-
+    win = 1;
+    count = 0;
 }
 
 function drawOnBoard(el, icon) {
@@ -30,63 +31,81 @@ function drawOnBoard(el, icon) {
 function checkWinner() {
     //check for rows
     if (boxes[0].classList.contains('fa-opera') && boxes[1].classList.contains('fa-opera') && boxes[2].classList.contains('fa-opera')) {
-        showMessage("Red Win");
+        showMessage("Win", 'opera');
+
     }
     else if (boxes[3].classList.contains('fa-opera') && boxes[4].classList.contains('fa-opera') && boxes[5].classList.contains('fa-opera')) {
-        showMessage("Red Win");
+        showMessage("Win", 'opera');
+
     }
     else if (boxes[6].classList.contains('fa-opera') && boxes[7].classList.contains('fa-opera') && boxes[8].classList.contains('fa-opera')) {
-        showMessage("Red Win");
+        showMessage("Win", 'opera');
+
     }
-    if (boxes[0].classList.contains('fa-xbox') && boxes[1].classList.contains('fa-xbox') && boxes[2].classList.contains('fa-xbox')) {
-        showMessage("Green Win");
+    else if (boxes[0].classList.contains('fa-xbox') && boxes[1].classList.contains('fa-xbox') && boxes[2].classList.contains('fa-xbox')) {
+        showMessage("Win ", 'xbox');
+
     }
     else if (boxes[3].classList.contains('fa-xbox') && boxes[4].classList.contains('fa-xbox') && boxes[5].classList.contains('fa-xbox')) {
-        showMessage("Green Win");
+        showMessage("Win ", 'xbox');
+
     }
     else if (boxes[6].classList.contains('fa-xbox') && boxes[7].classList.contains('fa-xbox') && boxes[8].classList.contains('fa-xbox')) {
-        showMessage("Green Win");
+        showMessage("Win ", 'xbox');
+
     }
 
 
     //check for columns
 
     else if (boxes[0].classList.contains('fa-opera') && boxes[3].classList.contains('fa-opera') && boxes[6].classList.contains('fa-opera')) {
-        showMessage("Red Win");
+        showMessage("Win", 'opera');
+
     }
     else if (boxes[1].classList.contains('fa-opera') && boxes[4].classList.contains('fa-opera') && boxes[7].classList.contains('fa-opera')) {
-        showMessage("Red Win");
+        showMessage("Win", 'opera');
+
     }
     else if (boxes[2].classList.contains('fa-opera') && boxes[5].classList.contains('fa-opera') && boxes[8].classList.contains('fa-opera')) {
-        showMessage("Red Win");
+        showMessage("Win", 'opera');
+
     }
 
     else if (boxes[0].classList.contains('fa-xbox') && boxes[3].classList.contains('fa-xbox') && boxes[6].classList.contains('fa-xbox')) {
-        showMessage("Green Win");
+        showMessage("Win ", 'xbox');
+
     }
     else if (boxes[1].classList.contains('fa-xbox') && boxes[4].classList.contains('fa-xbox') && boxes[7].classList.contains('fa-xbox')) {
-        showMessage("Green Win");
+        showMessage("Win ", 'xbox');
+
     }
     else if (boxes[2].classList.contains('fa-xbox') && boxes[5].classList.contains('fa-xbox') && boxes[8].classList.contains('fa-xbox')) {
-        showMessage("Green Win");
+        showMessage("Win ", 'xbox');
+
     }
     //check for diagonals
     else if (boxes[0].classList.contains('fa-opera') && boxes[4].classList.contains('fa-opera') && boxes[8].classList.contains('fa-opera')) {
-        showMessage("Red Win");
+        showMessage("Win", 'opera');
+
     }
     else if (boxes[2].classList.contains('fa-opera') && boxes[4].classList.contains('fa-opera') && boxes[6].classList.contains('fa-opera')) {
-        showMessage("Red Win");
+        showMessage("Win", 'opera');
+
     }
     else if (boxes[0].classList.contains('fa-xbox') && boxes[4].classList.contains('fa-xbox') && boxes[8].classList.contains('fa-xbox')) {
-        showMessage("Green Win");
+        showMessage("Win ", 'xbox');
+
     }
     else if (boxes[2].classList.contains('fa-xbox') && boxes[4].classList.contains('fa-xbox') && boxes[6].classList.contains('fa-xbox')) {
-        showMessage("Green Win");
+        showMessage("Win ", 'xbox');
+
     }
 
 }
 
 container.onclick = (e) => {
+
+
     if (player === 0) {
         if (!e.target.classList.contains('fab') && !e.target.classList.contains('fa-opera') && e.target.classList.contains('column')) {
             drawOnBoard(e.target, 'opera');
@@ -96,6 +115,7 @@ container.onclick = (e) => {
         checkWinner();
     }
     else if (player === 1) {
+
         if (!e.target.classList.contains('fab') && !e.target.classList.contains('fa-opera') && e.target.classList.contains('column')) {
             drawOnBoard(e.target, 'xbox');
             player = 0;
@@ -104,21 +124,19 @@ container.onclick = (e) => {
         checkWinner();
 
     }
-    console.log(count);
-    if (count === 9) {
-        showMessage('Draw')
+    if (count == 9 && win == 0) {
+        showMessage('Draw', 'draw');
+        count = 0;
     }
-
 }
 
 
 reset.onclick = (e) => {
     e.preventDefault();
-    console.log(e.target);
     boxes.forEach((box) => {
         box.className = 'column';
     })
-
+    count = 0;
+    win = 0;
 }
 
-console.log(boxes[0].className);
