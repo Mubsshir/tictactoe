6bdbf901075644661499777b1068a15745b9dea9
Mohd Mubsshir Khan <kmubsshir@hotmail.com>
2022-07-27 16:28:12 +0530

    remover useState hooks

--- src/App.js
+++ src/App.js
@@ -2,12 +2,9 @@ import { useState } from 'react';
 import './App.css';
 import Home from './component/Home';
 function App() {
-  const [haveWinner, setWinner] = useState(false)
 
   return (
     <div className="App">
-      {/* {haveWinner && <MessageBox />} */}
-      {/* <Board result={showResult} /> */}
       <Home />
     </div>
   );
